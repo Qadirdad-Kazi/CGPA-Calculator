e05e00cf2a845f1339628949f78530eefa13c98a
Qadirdad-Kazi <qadirdadkazi@gmail.com>
2025-02-12 03:08:59 +0500

    dep-4

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || "https://cgpa-calculator-wine.vercel.app/api/auth/login ";
+// export const API_URL = process.env.REACT_APP_API_URL || "https://cgpa-calculator-wine.vercel.app/api/auth/login ";
+export const API_URL = "https://cgpa-calculator-wine.vercel.app/api";
