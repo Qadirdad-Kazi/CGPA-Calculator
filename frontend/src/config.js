5daec62b23a1c1ba51981087ad4428e998da3d83
Qadirdad-Kazi <154679341+Qadirdad-Kazi@users.noreply.github.com>
2025-08-15 00:54:38 +0500

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,1 @@
-// export const API_URL = process.env.REACT_APP_API_URL || "https://cgpa-calculator-wine.vercel.app/api/auth/login ";
-export const API_URL = "https://cgpa-calculator-wine.vercel.app/api";
+export const API_URL = process.env.REACT_APP_API_URL || "https://cgpa-calculator-wine.vercel.app/api";
